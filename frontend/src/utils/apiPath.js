014dc84af80396bc85a4e5360b24f09518f00cd0
Ashish Jha <ashishjhag143@gmail.com>
2025-08-12 15:43:48 +0530

    adding apiPaths in frontend

---
+++ frontend/src/utils/apiPath.js
@@ -0,0 +1,30 @@
+
+export const API_BASE_URL = "https://localhost:4000";
+
+export const API_PATHS = {
+  auth: {
+    login: "/api/auth/login",
+    register: "/api/auth/register",
+    getProfile: "/api/auth/profile",
+  },
+  image: {
+    upload_img: "/api/auth/upload-image",
+  },
+  sessions: {
+    create: "/api/sessions/create",
+    getAll: "/api/sessions/my-sessions",
+    getOne: (id) => `/api/sessions/${id}`,
+    // update: (id) => `/api/sessions/update/${id}`,
+    delete: (id) => `/api/sessions/${id}`,
+  },
+  questions: {
+    add_to_session: "/api/questions/add",
+    // getById: (id) => `/api/questions/${id}`,
+    pin: (id) => `/api/questions/${id}/pin`,
+    update_note: (id) => `/api/questions/${id}/note`,
+  },
+  ai: {
+    generateQuestions: "/api/ai/generate-questions",
+    generateExplanations: "/api/ai/generate-explainations",
+  },
+};
